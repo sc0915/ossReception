d52f0b4708381aad83464dd9ff4c5053f3a864d4
SC <sunchaoo915@163.com>
2014-12-30 16:38:23 +0800

    修改拦截器

--- src/main/java/com/shinowit/web/MerchandiseinfoController.java
+++ src/main/java/com/shinowit/web/MerchandiseinfoController.java
@@ -1,19 +1,11 @@
 package com.shinowit.web;
 
-import com.shinowit.dao.mapper.TmeMerchandiseinfoMapper;
 import com.shinowit.framework.dao.MerchandiseinfoDAO;
-import com.shinowit.model.TmeMerchandiseinfo;
-import com.shinowit.model.TmeMerchandiseinfoExample;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
